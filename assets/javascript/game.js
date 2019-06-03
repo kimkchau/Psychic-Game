1a58bba8b6337e816cf7472e34ca157b1338a819
Kim C <kimkly.chau@gmail.com>
2019-06-03 16:01:04 -0700

    added additional code to resolve guessesLeft and pastUserGuesses variables and added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,54 @@
- // Creates an array that lists out all of the options (a...z).
- var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j','k','l','m','n', 'o', 'p','q','r','s','t','u','v', 'w', 'x','y','z'];
+// Creates an array that lists out all of the options (a...z).
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
- // Creating variables to hold the number of wins, losses, guessesLeft, totalGuess. 
- var wins = 0;
- var losses = 0;
- var guessesLeft = 9;
- var totalGuess = userChoiceText;
- var pastUserGuesses = "";
- 
- // Create variables that hold references to the places in the HTML where we want to display things.
- var winsText = document.getElementById("wins-text");
- var lossesText = document.getElementById("losses-text");
- var guessesLeft = document.getElementById("guessesLeft");
- var userChoiceText = document.getElementById("userchoice-text");
- 
+// Creating variables to hold the number of wins, losses, guessesLeft, totalGuess. 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var pastUserGuesses = "";
 
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log(computerGuess);
-  
-  for (var i=1; i<10; i++){
-    
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-      
-      
-      
-      // Determines which key was pressed by USER.
-      var userGuess = event.key;
-      console.log(userGuess);
-      pastUserGuesses = pastUserGuesses + userGuess + ", " 
-      
-      
-      
-      // This logic determines the outcome of the game (win/loss), and increments the appropriate number
-      if (userGuess === computerGuess) {
-        wins++;
-        i=10;} 
-        else if (i === 9) {
-          losses++;}
-          
-          // Display the user wins/losses, guesses left, and guesses so far.
-          winsText.textContent = "wins: " + wins;
-          lossesText.textContent = "losses: " + losses;
-          guessesLeft.textContent = "Guesses Left: " + guessesLeft - i ;
-          userChoiceText.textContent = "Your Guesses so far: " + pastUserGuesses;
-          
-        }
-        
-        
-      }
-      ;
+// Create variables that hold references to the places in the HTML where we want to display things.
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftDiv = document.getElementById("guessesLeft");
+var userChoiceText = document.getElementById("userchoice-text");
+
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+  guessesLeft--;
+  console.log(guessesLeft);
+
+  // Determines which key was pressed by USER.
+  var userGuess = event.key;
+  console.log(userGuess);
+  pastUserGuesses = pastUserGuesses + userGuess + ", "
+
+  // This logic determines the outcome of the game (win/loss), and increments the appropriate number
+  if (userGuess === computerGuess) {
+    wins++;
+    guessesLeft = 9;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+    pastUserGuesses="";
+  }
+
+  else if (guessesLeft === 0) {
+    losses++;
+    guessesLeft = 9;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+    pastUserGuesses=""
+
+  }
+
+  // Display the user wins/losses, guesses left, and guesses so far.
+  winsText.textContent = "wins: " + wins;
+  lossesText.textContent = "losses: " + losses;
+  guessesLeftDiv.textContent = "Guesses Left: " + guessesLeft;
+  userChoiceText.textContent = "Your Guesses so far: " + pastUserGuesses;
+
+};
